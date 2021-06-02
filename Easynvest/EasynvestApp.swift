1fc87648235a9d5a24e444d557c78098fc3df9a8
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-02 18:08:26 -0300

    Initial Commit

---
+++ Easynvest/EasynvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasynvestApp.swift
+//  Easynvest
+//
+//  Created by Raphael Cerqueira on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct EasynvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
